c6e699a32537683e07cabe59fe96751d5746ec3d
Saby <kotofeykoty@gmail.com>
2022-03-26 21:19:23 +0300

    sprint3inc8,9,DB

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,16 +2,14 @@ package config
 
 import (
 	"flag"
-	"log"
-
-	"github.com/caarlos0/env"
+	"os"
 )
 
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"test.txt"`
-	DatabaseDSN     string `env:"DATABASE_DSN" envDefault:"user=postgres password=password database=url sslmode=disable"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
 }
 
 func ParseVariables() Config {
@@ -21,15 +19,37 @@ func ParseVariables() Config {
 		FileStoragePath: "test.txt",
 	}
 
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Fatal(err)
+	// err := env.Parse(&cfg)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	sa := os.Getenv("SERVER_ADDRESS")
+	if sa != "" {
+		cfg.ServerAddress = sa
+	}
+
+	bu := os.Getenv("BASE_URL")
+	if bu != "" {
+		cfg.BaseURL = bu
+	}
+
+	fsp := os.Getenv("FILE_STORAGE_PATH")
+	if fsp != "" {
+
+		cfg.FileStoragePath = fsp
+	}
+
+	dd, ok := os.LookupEnv("DATABASE_DSN")
+	if ok {
+
+		cfg.DatabaseDSN = dd
 	}
 
 	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "Server address")
 	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "Base URL")
 	flag.StringVar(&cfg.FileStoragePath, "f", cfg.FileStoragePath, "file storage path")
-	flag.StringVar(&cfg.DatabaseDSN, "d", cfg.DatabaseDSN, "Database")
+	flag.StringVar(&cfg.DatabaseDSN, "d", "", `database dsn (default "")`)
 	flag.Parse()
 
 	return cfg
