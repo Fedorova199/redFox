e571c0c2486859de43a9f20fc48fbc49716e1a0f
Saby <kotofeykoty@gmail.com>
2022-03-27 14:35:28 +0300

    sprint3inc8,9,DB

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,34 +2,26 @@ package main
 
 import (
 	"database/sql"
-	"flag"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/Fedorova199/redfox/internal/app/config"
 	"github.com/Fedorova199/redfox/internal/app/handlers"
 	"github.com/Fedorova199/redfox/internal/app/interfaces"
 	"github.com/Fedorova199/redfox/internal/app/middlewares"
 	"github.com/Fedorova199/redfox/internal/app/storage"
-	"github.com/caarlos0/env"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS"`
-	BaseURL         string `env:"BASE_URL"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-	DatabaseDSN     string `env:"DATABASE_DSN"`
-}
-
 func main() {
-	cfg := parseVariables()
-	//cfg, err := config.NewConfig()
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	db, err := sql.Open("pgx", cfg.DatabaseDSN)
 	if err != nil {
 		log.Fatalln(err)
@@ -66,25 +58,4 @@ func main() {
 	}()
 
 	log.Fatal(server.ListenAndServe())
-}
-
-func parseVariables() Config {
-	var cfg = Config{
-		ServerAddress:   ":8080",
-		BaseURL:         "http://localhost:8080",
-		FileStoragePath: "test.txt",
-	}
-
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "Server address")
-	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "Base URL")
-	flag.StringVar(&cfg.FileStoragePath, "f", cfg.FileStoragePath, "File storage path")
-	flag.StringVar(&cfg.DatabaseDSN, "d", cfg.DatabaseDSN, "Database DSN")
-	flag.Parse()
-
-	return cfg
 }
--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,76 +1,76 @@
 package config
 
-// import (
-// 	"flag"
-// 	"os"
-// 	"strings"
-// )
+import (
+	"flag"
+	"os"
+	"strings"
+)
 
-// type Config struct {
-// 	ServerAddress   string `env:"SERVER_ADDRESS"`
-// 	BaseURL         string `env:"BASE_URL"`
-// 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-// 	DatabaseDSN     string `env:"DATABASE_DSN"`
-// }
+type Config struct {
+	ServerAddress   string `env:"SERVER_ADDRESS"`
+	BaseURL         string `env:"BASE_URL"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
+}
 
-// const (
-// 	defaultServerAddress = ":8080"
-// 	defaultBaseURL       = "http://localhost:8080"
-// )
+const (
+	defaultServerAddress = ":8080"
+	defaultBaseURL       = "http://localhost:8080"
+)
 
-// var defaultConfig = Config{
-// 	ServerAddress: defaultServerAddress,
-// 	BaseURL:       defaultBaseURL,
-// }
+var defaultConfig = Config{
+	ServerAddress: defaultServerAddress,
+	BaseURL:       defaultBaseURL,
+}
 
-// func NewConfig() (Config, error) {
-// 	conf := defaultConfig
-// 	conf.parseEnvVars()
-// 	conf.parseFlags()
+func NewConfig() (Config, error) {
+	conf := defaultConfig
+	conf.parseEnvVars()
+	conf.parseFlags()
 
-// 	err := conf.Validate()
-// 	return conf, err
-// }
+	err := conf.Validate()
+	return conf, err
+}
 
-// func (conf *Config) parseFlags() {
+func (conf *Config) parseFlags() {
 
-// 	flag.StringVar(&conf.ServerAddress, "a", defaultServerAddress, "network address the server listens on")
-// 	flag.StringVar(&conf.BaseURL, "b", defaultBaseURL, "resulting base URL")
-// 	flag.StringVar(&conf.FileStoragePath, "f", "test.txt", "storage file")
-// 	flag.StringVar(&conf.DatabaseDSN, "d", "", `database dsn (default "")`)
-// 	flag.Parse()
+	flag.StringVar(&conf.ServerAddress, "a", defaultServerAddress, "network address the server listens on")
+	flag.StringVar(&conf.BaseURL, "b", defaultBaseURL, "resulting base URL")
+	flag.StringVar(&conf.FileStoragePath, "f", "test.txt", "storage file")
+	flag.StringVar(&conf.DatabaseDSN, "d", "", `database dsn (default "")`)
+	flag.Parse()
 
-// }
+}
 
-// func (conf *Config) parseEnvVars() {
-// 	sa := os.Getenv("SERVER_ADDRESS")
-// 	if sa != "" {
-// 		conf.ServerAddress = sa
-// 	}
+func (conf *Config) parseEnvVars() {
+	sa := os.Getenv("SERVER_ADDRESS")
+	if sa != "" {
+		conf.ServerAddress = sa
+	}
 
-// 	bu := os.Getenv("BASE_URL")
-// 	if bu != "" {
-// 		conf.BaseURL = bu
-// 	}
+	bu := os.Getenv("BASE_URL")
+	if bu != "" {
+		conf.BaseURL = bu
+	}
 
-// 	fsp := os.Getenv("FILE_STORAGE_PATH")
-// 	if fsp != "" {
+	fsp := os.Getenv("FILE_STORAGE_PATH")
+	if fsp != "" {
 
-// 		conf.FileStoragePath = fsp
-// 	}
+		conf.FileStoragePath = fsp
+	}
 
-// 	dd, ok := os.LookupEnv("DATABASE_DSN")
-// 	if ok {
+	dd, ok := os.LookupEnv("DATABASE_DSN")
+	if ok {
 
-// 		conf.DatabaseDSN = dd
-// 	}
-// }
+		conf.DatabaseDSN = dd
+	}
+}
 
-// func (conf *Config) Validate() error {
+func (conf *Config) Validate() error {
 
-// 	conf.ServerAddress = strings.TrimSpace(conf.ServerAddress)
-// 	conf.BaseURL = strings.TrimSpace(conf.BaseURL)
-// 	conf.FileStoragePath = strings.TrimSpace(conf.FileStoragePath)
+	conf.ServerAddress = strings.TrimSpace(conf.ServerAddress)
+	conf.BaseURL = strings.TrimSpace(conf.BaseURL)
+	conf.FileStoragePath = strings.TrimSpace(conf.FileStoragePath)
 
-// 	return nil
-// }
+	return nil
+}
